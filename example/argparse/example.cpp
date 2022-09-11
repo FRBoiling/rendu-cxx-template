e5a575b0093e08246f1b5336968ef76562fba878
boil <free22858@live.com>
2022-09-12 01:39:12 +0800

    example[例子] 添加argparse使用例子
    example[例子] 添加magice_enum使用例子

--- example/argparse/example.cpp
+++ example/argparse/example.cpp
@@ -4,13 +4,13 @@
 
 #include <argparse/argparse.hpp>
 #include <string>
-
-int main(int argc, char ** argv) {
+//例如： 1 2 3 -abc 4.14 5.718 -gh 6 7
+int main(int argc, char **argv) {
   argparse::ArgumentParser program("test");
 
-//  program.add_argument("numbers")
-//      .nargs(3)
-//      .scan<'i', int>();
+  program.add_argument("numbers")
+      .nargs(3)
+      .scan<'i', int>();
 
   program.add_argument("-a")
       .default_value(false)
@@ -27,16 +27,17 @@ int main(int argc, char ** argv) {
   program.add_argument("--files")
       .nargs(3);
 
-  program.add_argument("-n1").required()
+  program.add_argument("-g").required()
       .default_value(0)
-  .scan<'i', int>();
-  program.add_argument("-n2").required()
+      .scan<'i', int>();
+  program.add_argument("-h").required()
       .default_value(0)
-  .scan<'i', int>();
+      .scan<'i', int>();
+
   try {
     program.parse_args(argc, argv);
   }
-  catch (const std::runtime_error& err) {
+  catch (const std::runtime_error &err) {
     std::cerr << err.what() << std::endl;
     std::cerr << program;
     std::exit(1);
@@ -44,11 +45,10 @@ int main(int argc, char ** argv) {
 //  auto numbers = program.get<std::vector<int>>("numbers");        // {1, 2, 3}
   auto a = program.get<bool>("-a");                               // true
   auto b = program.get<bool>("-b");                               // true
-  auto c = program.get<std::vector<float>>("-c");                 // {3.14f, 2.718f}
+  auto c = program.get<std::vector<float>>("-c");                 // {4.14f, 5.718f}
   auto files = program.get<std::vector<std::string>>("--files");  // {"a.txt", "b.txt", "c.txt"}
-  auto g = program.get<int>("-n1");
-  auto h = program.get<int>("-n2");
+  auto g = program.get<int>("-g");             // 6
+  auto h = program.get<int>("-h");             // 7
 
-  std::string str_cin;
-  std::cin >> str_cin;
+  std::cout<<program;
 }
--- example/magic_enum/example.cpp
+++ example/magic_enum/example.cpp
@@ -24,9 +24,19 @@ int main(int, char *[]) {
   int a = magic_enum::enum_integer(EnumClass::Num1);
   int b = magic_enum::enum_integer(EnumNormal::Num1);
 
-  auto c = magic_enum::enum_cast<EnumNormal>("");
+  auto c = magic_enum::enum_cast<EnumNormal>(1);
 
   auto d =  c.value();
   auto e = c.has_value();
+
+  auto f1 = magic_enum::enum_integer(EnumClass::Num1);
+  auto f2 = magic_enum::enum_index(EnumClass::Num1);
+
+  auto g = magic_enum::enum_contains<EnumClass>(EnumClass::Num1);
+
+  auto h = magic_enum::enum_flags_name(EnumClass::Num1);
+
+  auto i = magic_enum::enum_name(EnumClass::Num1);
+
   return 0;
 }
