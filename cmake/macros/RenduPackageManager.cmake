86cabe2612d10536183765162f3f7d4f0a4d9a4b
Boil <free22858@live.com>
2025-10-21 17:11:51 +0800

    🦄 refactor(项目结构): cmake 代码整理

--- cmake/macros/RenduPackageManager.cmake
+++ cmake/macros/RenduPackageManager.cmake
@@ -1152,7 +1152,7 @@ function(rendu_add_package)
                     "${ARGS_OPTIONS}"
             )
         endif ()
-        _get_fetch_properties("${ARGS_NAME}")
+        _get_fetch_properties(${pkgName} sourceDir binaryDir)
     endif ()
 
     set(${ARGS_NAME}_ADDED YES)
